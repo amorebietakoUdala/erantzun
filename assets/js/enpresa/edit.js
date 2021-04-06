dd1d344ce43f867a39a332a1ff70edae0bc6824e
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2021-04-06 12:43:50 +0200

    Removed a comment

--- assets/js/enpresa/edit.js
+++ assets/js/enpresa/edit.js
@@ -3,7 +3,6 @@ import '../../scss/enpresa/edit.scss';
 import $ from 'jquery';
 const routes = require('../../../public/js/fos_js_routes.json');
 import Routing from '../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
-//import 'urijs/src/URI.min.js';
 
 $(function() {
     Routing.setRoutingData(routes);
